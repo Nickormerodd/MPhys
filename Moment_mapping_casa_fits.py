45b02bb04abef7779bf91134da93c590c5f52551
Nickormerodd <117305679+Nickormerodd@users.noreply.github.com>
2023-10-13 11:55:38 +0100

    Update Moment_mapping_casa_fits.py
    i added on the 0,1,6,7,8 moments - the 6,7,8 need to have different regions (not [10,1000] but maybe [3,1000] otherwise they're tiny)

--- Moment_mapping_casa_fits.py
+++ Moment_mapping_casa_fits.py
@@ -6,7 +6,7 @@ Created on Tue Oct 10 15:57:51 2023
 
 import numpy as np
 from astropy.io import fits
-from spectral_cube import SpectralCube as sc
+#from spectral_cube import SpectralCube as sc
 from tkinter import Tk
 from tkinter import filedialog
 import matplotlib.pyplot as plt
@@ -15,39 +15,47 @@ import os
 
 def get_data():
 
-    Tk().withdraw()  
+    Tk().withdraw()
     file_paths = filedialog.askopenfilenames()
     for file_path in file_paths:
         data = fits.open(file_path)
         filename = (os.path.basename(file_path).replace(".fits", ""))#.replace("_"," ").replace("."," ")
         main(data, filename, file_path)
-        data.close()    
+        data.close()
     return
-                                     
+
 
 def main(data, name, path):
-    
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
+
     m = data[0].shape[1]
     p = data[0].shape[2]
-    
+
     print(data[0].shape)
 
     x = np.linspace(0, p - 1, p)
     y = np.linspace(0, m - 1, m)
     X, Y = np.meshgrid(x, y)
-    
+
     median = np.nanmedian(data[0].data[0])
     print(median)
-    
+
     name = name.replace("_image","") + ".png"
     print(name)
-    
+
     calibration_freq = 220.70908 #GHz
-    
-    if '914' in name.lower():
+
+    if '875' in name.lower():
+        title = '$CH_{3}CN-J_{K}=12_{0}\\rightarrow11_{0}$'
+        conversion = (c/1000) * (220.747369 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+    elif '890' in name.lower():
+        title = '$CH_{3}CN-J_{K}=12_{1}\\rightarrow11_{1}$'
+        conversion = (c/1000) * (220.743012 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+    elif '914' in name.lower():
         title = '$CH_{3}CN-J_{K}=12_{2}\\rightarrow11_{2}$'
         conversion = (c/1000) * (220.73027 - calibration_freq)/calibration_freq
         data[0].data[0] = data[0].data[0] + conversion
@@ -61,23 +69,35 @@ def main(data, name, path):
         title = '$CH_{3}CN-J_{K}=12_{5}\\rightarrow11_{5}$'
         conversion = (c/1000) * (220.64112 - calibration_freq)/calibration_freq
         data[0].data[0] = data[0].data[0] + conversion
+    elif '1190' in name.lower():
+        title = '$CH_{3}CN-J_{K}=12_{6}\\rightarrow11_{6}$'
+        conversion = (c/1000) * (220.593987 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+    elif '1300' in name.lower():
+        title = '$CH_{3}CN-J_{K}=12_{7}\\rightarrow11_{7}$'
+        conversion = (c/1000) * (220.538635 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+    elif '1430' in name.lower():
+        title = '$CH_{3}CN-J_{K}=12_{8}\\rightarrow11_{8}$'
+        conversion = (c/1000) * (220.474767 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
     else:
         title = 'unknown'
-    
+
     contourf = ax.contourf(X, Y, data[0].data[0] , cmap='bwr',
                            levels = 50) # 'viridis')
     plt.colorbar(contourf, label='km/s')
     ax.set_title(title)
-    
+
     ax.set_xticks([])
     ax.set_yticks([])
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
-    
+
     plt.savefig(name, dpi=1000)
-    
+
     return
 
 get_data()
