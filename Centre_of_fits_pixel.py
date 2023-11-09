94d01bd024e10f9f7555e4efa75894760c88bf3c
hiddenwife <147258059+hiddenwife@users.noreply.github.com>
2023-11-09 12:16:57 +0000

    Update Centre_of_fits_pixel.py

--- Centre_of_fits_pixel.py
+++ Centre_of_fits_pixel.py
@@ -12,6 +12,9 @@ from tkinter import filedialog
 import os
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+#from mpl_toolkits.mplot3d import Axes3D as mpl
+from matplotlib.lines import Line2D
+
 
 
 def get_data():
@@ -22,7 +25,7 @@ def get_data():
         data = fits.open(file_path)
         filename = (os.path.basename(file_path).replace(".fits", "")).replace(".image.pbcor_line.galactic","")
         main(data, filename, file_path)
-        #main_2(data)
+        main_2(data, filename, file_path)
         data.close()
         
     return
@@ -30,6 +33,7 @@ def get_data():
 def main(data, filename, path):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    #mpl.rcParams['legend.fontsize'] = 10
 
     num_channels = data[0].shape[0]
     shape = (data[0].shape[1], data[0].shape[2])
@@ -66,15 +70,68 @@ def main(data, filename, path):
     # Generate the fitted surface using the parameters obtained
     fitted_surface = func(xdata, *popt).reshape(m, p)
 
-    surf = ax.plot_surface(X, Y, fitted_surface, cmap='viridis', linewidth=0, antialiased=False)
-
+    surf = ax.plot_surface(X, Y, fitted_surface, cmap='viridis', 
+                           linewidth=0, antialiased=False)
+    surf_2 = ax.plot_surface(X, Y, temp, cmap='magma',
+                             linewidth=0, antialiased=False)
+    
     # Customize the plot (labels, title, etc.) as needed
     ax.set_title('Location of Protostar - Gaussian Fit ' + filename, size = 8)
 
     # Add a colorbar
-    #fig.colorbar(surf, ax=ax, shrink=0.5, aspect=5, label='K')
-
+    fig.colorbar(surf, ax=ax, shrink=0.5, aspect=5, label='gaus')
+    fig.colorbar(surf_2, ax=ax, shrink=0.5, aspect=5, label='data')
+    
     plt.show()
+
+def main_2(data, filename, path):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    num_channels = data[0].shape[0]
+    shape = (data[0].shape[1], data[0].shape[2])
+    temp = np.zeros(shape)
+
+    for i in range(num_channels):
+        temp = temp + data[0].data[i]
+
+    # Clean the data by replacing NaNs with zeros
+    temp = temp/num_channels
+    temp[np.isnan(temp)] = 0
+
+    m, p = temp.shape
+    X, Y = np.meshgrid(range(p), range(m))
+    xdata = np.column_stack((X.ravel(), Y.ravel()))
+
+    def func(xdata, A, mean_x, mean_y, sigma):
+        x, y = xdata[:, 0], xdata[:, 1]
+        return A * np.exp(-((x - mean_x) ** 2 + (y - mean_y) ** 2) / (2 * sigma ** 2))
+
+    # Initial guess for the parameters
+    initial_guess = (temp.max(), np.argmax(temp) % temp.shape[1], np.argmax(temp) // temp.shape[1], 1.0)
+
+    popt, _ = curve_fit(func, xdata, temp.ravel(), p0=initial_guess)
+
+    # Extract the (x, y) coordinates of the Gaussian fit's maximum
+    new_max_x, new_max_y = int(popt[1]), int(popt[2])
+
+    max_x,max_y = find_max_value_coordinates(temp)
+    fitted_surface = func(xdata, *popt).reshape(m, p)
+
+    surf = ax.contour(X, Y, fitted_surface, cmap='viridis',levels = 20,alpha=0.5)
+    surf_2 = ax.contour(X, Y, temp, cmap='magma',levels=20,alpha=0.7)
+    ax.scatter(max_x,max_y, label = 'max value',color='green')
+    ax.scatter(new_max_x,new_max_y,label='gaus max value', color='red')
+    
+    # Customize the plot (labels, title, etc.) as needed
+    ax.set_title('Location of Protostar - Gaussian Fit ' + filename, size = 8)
+
+    ax.legend()
+    plt.show()
+    
+    return
+
 
 def find_max_value_coordinates(arr):
     max_value = np.nanmax(arr)
