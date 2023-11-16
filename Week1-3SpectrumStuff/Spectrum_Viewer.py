3b8bd37f6627d4b32f62ed19700ae19c99e0bd9b
hiddenwife <147258059+hiddenwife@users.noreply.github.com>
2023-11-16 16:15:22 +0000

    Update Spectrum_Viewer.py

--- Week1-3SpectrumStuff/Spectrum_Viewer.py
+++ Week1-3SpectrumStuff/Spectrum_Viewer.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Oct 12 13:58:39 2023
 
-@author: Chr
+@author: Christopher
 """
 
 import numpy as np
@@ -13,17 +13,17 @@ from tkinter import Tk
 from tkinter import filedialog
 import matplotlib.pyplot as plt
 import os
-#import radio_beam as beam
-#from scipy.constants import c
-#from astropy import units as u
-#from scipy.optimize import curve_fit
-#from matplotlib.ticker import FuncFormatter
-#import uncertainties.unumpy as unp
-#from uncertainties import ufloat
+import radio_beam as beam
+from scipy.constants import c
+from astropy import units as u
+from scipy.optimize import curve_fit
+from matplotlib.ticker import FuncFormatter
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
 
 def get_data():
 
-    Tk().withdraw()
+    Tk().withdraw()  
     file_paths = filedialog.askopenfilenames()
     n = 0
     temp = []
@@ -32,31 +32,34 @@ def get_data():
         vel = []
         n = n + 1
         data = fits.open(file_path)
-        cube = sc.read(data)
         filename = (os.path.basename(file_path).replace(".fits", "")).replace("_"," ").replace("."," ")
         #velocity_name = main(data, filename, file_path, vel, 0, 0)
         #temp.append(velocity_name)
-        main(data, file_path, filename)
+        main(data,filename)
         data.close()
-
+        #print(file_path)
+        
     return
 
-def main(data, file_path, filename):
+def main(data, filename):
     print('Shape = ' + str(data[0].shape))
     num_channels = data[0].shape[0]
     header = data[0].header
+    print(data[0].shape)
     ref_freq = header['CRVAL3'] #reference frequency
     incr_freq = header['CDELT3'] #frequency increment
     freq = ((ref_freq + incr_freq * (np.arange(num_channels))) / 10**9 )
-
-    temp = []
+    
+    temp = []    
     for i in range(num_channels):
         max_value = np.nanmean(data[0].data[i]), freq[i]
         temp.append(max_value)
-
-
+    
+    
     channel_no = np.linspace(0,num_channels-1,num_channels)
     spectral = np.hstack((np.vstack((channel_no)),np.vstack((temp))))
+    
+    print('Mean = ' + str(np.mean(np.sqrt(spectral[:,1]**2))))
     #print(spectral)
     #spectrum(spectral, filename)
     """
@@ -68,45 +71,98 @@ def main(data, file_path, filename):
         val_2 = float(val_list[1])
     else:
         print("Invalid input format. Please use the format 'start-end'.")
-
+        
     print('Range = {}-{}'.format(val_1,val_2))
-
+    
     #data = filter_data(spectral,val_1,val_2)
     """
     spectrum(spectral, filename)
+    cube = sc.read(data)
+    
+    spectrum(spectral, filename)
     cube = DaskSpectralCube.read(data)
     stats = cube.statistics()
-    rms = stats['rms']
+    rms = stats['rms'].value
+    epic = float(rms)
+    print(type(epic))
+    print(epic)
+    #print(type(rms))
     print(f'RMS: {rms}')
+    print(f'RMS: {rms}', rms**2)
+    
+    epic_rms = np.mean(np.sqrt(spectral[:,1]**2))
+    
+    cube_data = cube.filled_data[:]
+    epicals = np.hstack((np.vstack((channel_no)),np.vstack((temp))))
+    epicals[:,1] = np.sqrt(epicals[:,1]**2)
+    
+    print(epicals)
+    
+    spectrum_2(spectral, epicals, filename, epic_rms)
+    #print(type(rms))
     return
 
 
 def spectrum(data, filename):
     #fig = plt.figure()
     #ax = fig.add_subplot()
-    fig, ax = plt.subplots(figsize=(12, 6))
-
+    fig, ax = plt.subplots(figsize=(12, 6)) 
+    
     ax.plot(data[:,2], data[:,1], linewidth=1)
     ax.set_title(filename)
     ax.set_xlabel('Frequency, GHz')
     ax.set_ylabel('Brightness temperature, K')
     ax.tick_params(axis='x', labelsize=9)
     ax.tick_params(axis='y', labelsize=9)
-
+    
     ax2 = ax.twiny()
     num_ticks_to_display = 15
     step_size = len(data[:, 0]) // num_ticks_to_display
-    channel_numbers = data[::step_size, 0][::-1]
+    channel_numbers = data[::step_size, 0][::-1] 
     ax2.set_xticks(data[::step_size, 0][::-1])
     ax2.set_xticklabels(channel_numbers.astype(int)[::-1])
     ax2.tick_params(axis='x', labelsize=9)
     ax2.set_xlabel('Channel number')
     ax2.spines['top'].set_color('none')
-
+    
     ax.grid(True, linewidth=0.5, alpha=0.7)
     filename = (filename.replace(" ", "_")).replace("image_pbcor_line_galactic_kelvin", "spectrum.png")
     print(filename)
     #plt.savefig(filename,dpi=1000)
+
+    return
+
+def spectrum_2(data, data_2, filename, rms):
+    #fig = plt.figure()
+    #ax = fig.add_subplot()
+    fig, ax = plt.subplots(figsize=(12, 6)) 
+    
+    ax.plot(data[:,2], data[:,1], linewidth=1, color='blue', label='Data')
+    ax.plot(data_2[:,2], data_2[:,1], linewidth=1, color='red', label = 'RMS Data')
+    ax.plot(data_2[:,2],np.linspace(rms,rms,len(data_2[:,1])),label='RMS Noise = {:.3f} K'.format(rms),
+            color='purple', linestyle='--', alpha=0.7)
+    ax.set_title(filename)
+    ax.set_xlabel('Frequency, GHz')
+    ax.set_ylabel('Brightness temperature, K')
+    ax.tick_params(axis='x', labelsize=9)
+    ax.tick_params(axis='y', labelsize=9)
+    """
+    ax2 = ax.twiny()
+    num_ticks_to_display = 15
+    step_size = len(data[:, 0]) // num_ticks_to_display
+    channel_numbers = data[::step_size, 0][::-1] 
+    ax2.set_xticks(data[::step_size, 0][::-1])
+    ax2.set_xticklabels(channel_numbers.astype(int)[::-1])
+    ax2.tick_params(axis='x', labelsize=9)
+    ax2.set_xlabel('Channel number')
+    ax2.spines['top'].set_color('none')
+    """
+    ax.grid(True, linewidth=0.5, alpha=0.7)
+    filename = (filename.replace(" ", "_")).replace("image_pbcor_line_galactic_kelvin", "spectrum.png")
+    print(filename)
+    ax.legend()
+    filename = filename + '_spectrum.png'
+    plt.savefig(filename,dpi=1000)
 
     return
 
@@ -115,7 +171,7 @@ def filter_data(data, val_1, val_2):
     first_column = data[:, 1]
     mask = (first_column >= val_1) & (first_column <= val_2)
     filtered_data = data[mask]
-
+    
     return filtered_data
 
 get_data()
