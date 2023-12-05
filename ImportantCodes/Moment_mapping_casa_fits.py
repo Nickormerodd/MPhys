7cc2af33add87019b39d52a3858bff392f6c08d6
Nickormerodd <117305679+Nickormerodd@users.noreply.github.com>
2023-12-05 13:04:22 +0000

    Update Moment_mapping_casa_fits.py

--- ImportantCodes/Moment_mapping_casa_fits.py
+++ ImportantCodes/Moment_mapping_casa_fits.py
@@ -16,19 +16,19 @@ import os
 
 def get_data():
 
-    Tk().withdraw()  
+    Tk().withdraw()
     file_paths = filedialog.askopenfilenames()
     for file_path in file_paths:
         data = fits.open(file_path)
         filename = (os.path.basename(file_path).replace(".fits", ""))#.replace("_"," ").replace("."," ")
         main(data, filename, file_path)
-        data.close()   
+        data.close()
         print(file_path)
-    
+
     #four_plot(file_paths)
-    
+
     return
-                                     
+
 
 def main(data, name, path):
 
@@ -46,12 +46,12 @@ def main(data, name, path):
     crval1 = header['CRVAL1']
     cdelt1 = header['CDELT1']
     crpix1 = header['CRPIX1']
-    
+
     ctype2 = header['CTYPE2']
     crval2 = header['CRVAL2']
     cdelt2 = header['CDELT2']
     crpix2 = header['CRPIX2']
-    
+
     # Create the coordinate arrays
     x = (crval1 + (np.arange(data[0].shape[2]) - crpix1) * cdelt1)
     y = (crval2 + (np.arange(data[0].shape[1]) - crpix2) * cdelt2)
@@ -113,29 +113,62 @@ def main(data, name, path):
         conversion = (c/1000) * (220.474767 - calibration_freq)/calibration_freq
         data[0].data[0] = data[0].data[0] + conversion
 
+    elif '_CH3_13CN_K=1' in name.upper():
+        title = '$CH_{3}(13)CN-J_{K}=12_{1}\\rightarrow11_{1}$'
+        k_number = 1
+        conversion = (c/1000) * (220.63384 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+
+    elif '_CH3_13CN_K=2' in name.upper():
+        title = '$CH_{3}(13)CN-J_{K}=12_{2}\\rightarrow11_{2}$'
+        k_number = 2
+        conversion = (c/1000) * (220.62113 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+
+    elif '_CH3_13CN_K=3' in name.upper():
+        title = '$CH_{3}(13)CN-J_{K}=12_{3}\\rightarrow11_{3}$'
+        k_number = 3
+        conversion = (c/1000) * (220.6000 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+
+    elif '_CH3_13CN_K=4' in name.upper():
+        title = '$CH_{3}(13)CN-J_{K}=12_{4}\\rightarrow11_{4}$'
+        k_number = 4
+        conversion = (c/1000) * (220.57032 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+
+    elif '_CH3_13CN_K=5' in name.upper():
+        title = '$CH_{3}(13)CN-J_{K}=12_{5}\\rightarrow11_{5}$'
+        k_number = 5
+        conversion = (c/1000) * (220.53224 - calibration_freq)/calibration_freq
+        data[0].data[0] = data[0].data[0] + conversion
+
     else:
         title = 'unknown'
         k_number = 'unknown'
 
-    name = "CH3CN_k=" + str(k_number) + "_gal" +".png"
+    if 'CH3_13CN' in name.upper():
+        name = "CH3(13)CN_k=" + str(k_number) + "_gal" +".png"
+    else:
+        name = "CH3CN_k=" + str(k_number) + "_gal" +".png"
     print(name)
-        
+
     x_au = x * 8.1 * 10**3 * 206264.81 * np.pi/180
     y_au = y * 8.1 * 10**3 * 206264.81 * np.pi/180
-    
+
     x_norm = np.abs(1000 / (8.1 * 10**3 * 206264.81 * np.pi/180))
     y_norm = np.abs(1000 / (8.1 * 10**3 * 206264.81 * np.pi/180))
-    
+
     #print(len(y))
     #print(np.mean(y))
-    
+
     x_point = np.min(x) + +(np.mean(x)/(len(x)*160))
     y_point = np.min(y)+(np.mean(y)/(len(y)*150))
     x_values = np.linspace(x_point, x_point+x_norm,2)
     y_values = np.linspace(y_point,y_point,2)
     ax.plot(x_values,y_values,color='black')#, label='1000 AU')
     ax.plot([],[],label='1000 AU', color='black', alpha=0)
-    
+
     #y_au = 1000
     y_norm = 1000 / (8.1 * 10**3 * 206264.81 * np.pi/180)
     x_center = x_point + x_norm / 2  # Center of the x-direction line
@@ -143,7 +176,7 @@ def main(data, name, path):
     x_values_y = np.linspace(x_center, x_center, 2)
     y_values_y = np.linspace(y_point, y_point + y_norm, 2)
     ax.plot(x_values_y, y_values_y, color='black')
-    
+
     arrow_props = dict(arrowstyle='->', linewidth=1, color='black')
     contourf = ax.contourf(x, y, data[0].data[0] , cmap='bwr',
                            levels = 50) # 'viridis')
@@ -152,8 +185,8 @@ def main(data, name, path):
     #centre = 48,46
     print(data[0].shape)
     #ax.scatter(x[48],y[46],label='centre')
-    
-    
+
+
     ax.set_xlabel("GLON deg")
     ax.set_ylabel("GLAT deg")
 
@@ -161,7 +194,7 @@ def main(data, name, path):
     #ax.invert_yaxis()  # Invert the y-axis
     ax.grid(alpha=0.2)
     ax.legend(loc='lower right', fontsize=7.5, borderaxespad=0.5, frameon=False, edgecolor='black')
-    
+
     plt.savefig(name, dpi=1000, bbox_inches='tight')
 
     return
